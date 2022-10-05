b64583684305a1c084165fa1c18353e5856de261
AungKhantNyar <ash@AungKhantNyars-MacBook-Air.local>
2022-10-05 10:29:48 +0630

    My first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function getRandom() {
+let start = document.getElementsByClassName('start')[0].value;
+let end = document.getElementsByClassName('end')[0].value;
+document.getElementsByClassName('number')[0].innerHTML = 'The random number is:' + getRandomBetween(parseInt(start),parseInt(end))
+}
+
+function getRandom(){
+    let start = document.getElementById('start').value;
+    let end = document.getElementById('end').value;
+    document.getElementById('number').innerHTML = 'The random number is: ' + getRandomBetween(parseInt(start),parseInt('end'))
+
+}
+
+function getRandomBetween(start, end){
+    return Math.floor(Math.random()  * (end - start + 1)) + start;
+}
